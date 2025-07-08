c6d3e4417a582aad7c4f3ff1cac99ae0a9c82394
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-08 17:49:44 +0200

    Added navigation based on NavHost

--- app/src/main/java/cat/rubenzu03/catbrary/MainActivity.kt
+++ app/src/main/java/cat/rubenzu03/catbrary/MainActivity.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.Icon;
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -27,11 +30,13 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
-var selectedScreen by mutableStateOf(Screen.Home)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,133 +44,144 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CatbraryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(),
-                    bottomBar =  { BottomNavigationBar()},
-                    topBar = { TopApplicationBar() }
+                val navController = rememberNavController()
+                Scaffold(
+                    topBar = { TopApplicationBar() },
+                    bottomBar = { BottomNavigationBar(navController) },
+                    floatingActionButton = { CreateFAB(navController) },
+                    floatingActionButtonPosition = FabPosition.End
                 ) { innerPadding ->
-                    when (selectedScreen) {
-                        Screen.Home -> MainScreen(Modifier.padding(innerPadding))
-                        Screen.Add -> AddCatForm(Modifier.padding(innerPadding))
-                        Screen.Favorites -> FavoritesScreen(Modifier.padding(innerPadding))
-                        Screen.Search -> SearchScreen(Modifier.padding(innerPadding))
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home",
+                        modifier = Modifier.padding(innerPadding),
+
+                        enterTransition = {
+                            fadeIn(animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing))
+                        },
+                        exitTransition = {
+                            fadeOut(animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing))
+                        },
+                        popEnterTransition = {
+                            fadeIn(animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing))
+                        },
+                        popExitTransition = {
+                            fadeOut(animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing))
+                        }
+
+                    ) {
+                        composable("home") { MainScreen() }
+                        composable("favorites") { FavoritesScreen() }
+                        composable("search") { SearchScreen() }
                     }
                 }
             }
         }
     }
-}
 
 
+    @Composable
+    fun BottomNavigationBar(navController: NavHostController) {
+        val backStack by navController.currentBackStackEntryAsState()
+        val currentRoute = backStack?.destination?.route
 
-@Preview
-@Composable
-fun BottomNavigationBar() {
-    NavigationBar {
-        NavigationBarItem(
-            selected = (selectedScreen == Screen.Home),
-            onClick = { selectedScreen = Screen.Home },
-            icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
-            label = { Text("Home") },
-            colors = NavigationBarItemDefaults.colors(
-                selectedIconColor = MaterialTheme.colorScheme.primary,
-                unselectedIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
-                selectedTextColor = MaterialTheme.colorScheme.primary,
-                unselectedTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+        NavigationBar {
+            NavigationBarItem(
+                selected = currentRoute == "home",
+                onClick = {
+                    navController.navigate("home") {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+                icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
+                label = { Text("Home") },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    unselectedTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                )
             )
-        )
-        NavigationBarItem(
-            selected = (selectedScreen == Screen.Add),
-            onClick = { selectedScreen = Screen.Add },
-            icon = { Icon(Icons.Default.Add, contentDescription = "Add") },
-            label = { Text("Add") },
-            colors = NavigationBarItemDefaults.colors(
-                selectedIconColor = MaterialTheme.colorScheme.primary,
-                unselectedIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
-                selectedTextColor = MaterialTheme.colorScheme.primary,
-                unselectedTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+
+            NavigationBarItem(
+                selected = currentRoute == "favorites",
+                onClick = {
+                    navController.navigate("favorites") {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+                icon = { Icon(Icons.Default.FavoriteBorder, contentDescription = "Favorites") },
+                label = { Text("Favorites") },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    unselectedTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                )
             )
-        )
-        NavigationBarItem(
-            selected = (selectedScreen == Screen.Favorites),
-            onClick =  { selectedScreen = Screen.Favorites },
-            icon = { Icon(Icons.Default.FavoriteBorder, contentDescription = "Favorites") },
-            label = { Text("Favorites") },
-            colors = NavigationBarItemDefaults.colors(
-                selectedIconColor = MaterialTheme.colorScheme.primary,
-                unselectedIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
-                selectedTextColor = MaterialTheme.colorScheme.primary,
-                unselectedTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+            NavigationBarItem(
+                selected = currentRoute == "search",
+                onClick = {
+                    navController.navigate("search") {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+                icon = { Icon(Icons.Default.Search, contentDescription = "Search") },
+                label = { Text("Search") },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    unselectedTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                )
             )
-        )
-        NavigationBarItem(
-            selected = (selectedScreen == Screen.Search),
-            onClick = { selectedScreen = Screen.Search },
-            icon = { Icon(Icons.Default.Search, contentDescription = "Search") },
-            label = { Text("Search") },
-            colors = NavigationBarItemDefaults.colors(
-                selectedIconColor = MaterialTheme.colorScheme.primary,
-                unselectedIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
-                selectedTextColor = MaterialTheme.colorScheme.primary,
-                unselectedTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
-            )
-        )
+        }
     }
-}
 
-@Preview
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TopApplicationBar(){
-    TopAppBar(
-        title = { Text("Catbrary") },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.primary
-        ),
-         /*TopAppBarDefaults.topAppBarColors(
+    @Preview
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TopApplicationBar() {
+        TopAppBar(
+            title = { Text("Catbrary") },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary
+            ),
+            /*TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f),
             titleContentColor = MaterialTheme.colorScheme.onPrimary
         ),*/
-    )
-}
+        )
+    }
 
 
+    @Composable
+    fun MainScreen(modifier: Modifier = Modifier) {
+        Text(
+            text = "Welcome to Catbrary!",
+            /* TYPOGRAPHY TEST style = MaterialTheme.typography.headlineLarge, */
+            modifier = modifier.padding(16.dp)
+        )
+    }
 
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Welcome to Catbrary!",
-        /* TYPOGRAPHY TEST style = MaterialTheme.typography.headlineLarge, */
-        modifier = modifier.padding(16.dp)
-    )
-}
 
-@Composable
-fun AddCatForm(modifier: Modifier = Modifier) {
-    Text(
-        text = "Add Cat Form",
-        modifier = modifier.padding(16.dp)
-    )
-}
+    @Composable
+    fun FavoritesScreen(modifier: Modifier = Modifier) {
+        Text(
+            text = "Favorites Screen",
+            modifier = modifier.padding(16.dp)
+        )
+    }
 
-@Composable
-fun FavoritesScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Favorites Screen",
-        modifier = modifier.padding(16.dp)
-    )
-}
-@Composable
-fun SearchScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Search Screen",
-        modifier = modifier.padding(16.dp)
-    )
-}
-
-enum class Screen {
-    Home,
-    Add,
-    Favorites,
-    Search
+    @Composable
+    fun SearchScreen(modifier: Modifier = Modifier) {
+        Text(
+            text = "Search Screen",
+            modifier = modifier.padding(16.dp)
+        )
+    }
 }
