192d8fe9c5275d833222fba1942baed3e0b2ab09
Ruben Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-22 02:08:17 +0200

    Create CatBreedInfo.kt

---
+++ app/src/main/java/cat/rubenzu03/catbrary/domain/CatBreedInfo.kt
@@ -0,0 +1,26 @@
+package cat.rubenzu03.catbrary.domain
+
+class CatBreedInfo {
+
+    var id: String = ""
+    var name: String = ""
+
+    var temperament: String = ""
+    var origin: String = ""
+    var description: String = ""
+    var indoor: Int = 0
+    var adaptability: Int = 0
+    var affectionLevel: Int = 0
+    var childFriendly: Int = 0
+    var dogFriendly: Int = 0
+    var energyLevel: Int = 0
+    var grooming: Int = 0
+    var healthIssues: Int = 0
+    var intelligence: Int = 0
+    var sheddingLevel: Int = 0
+    var socialNeeds: Int = 0
+    var strangerFriendly: Int = 0
+
+    var wikipediaUrl: String = ""
+    var ref_imageId: String = ""
+}
