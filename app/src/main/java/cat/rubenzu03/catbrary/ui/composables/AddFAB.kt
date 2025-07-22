8f851f2450a19b6b12eecd20847e0b9c23a21367
Ruben Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-22 18:32:55 +0200

    Updated FAB text to use strings.xml

--- app/src/main/java/cat/rubenzu03/catbrary/ui/composables/AddFAB.kt
+++ app/src/main/java/cat/rubenzu03/catbrary/ui/composables/AddFAB.kt
@@ -12,8 +12,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import cat.rubenzu03.catbrary.R
 import cat.rubenzu03.catbrary.persistence.CatRepository
 import cat.rubenzu03.catbrary.ui.viewmodel.CreateCatViewModel
 
@@ -38,7 +40,7 @@ fun CreateFAB(viewModel: CreateCatViewModel) {
         containerColor = MaterialTheme.colorScheme.secondaryContainer,
         contentColor   = MaterialTheme.colorScheme.primary,
         icon = { Icon(Icons.Default.Add, contentDescription = "Add") },
-        text = { Text("Add Cat") }
+        text = { Text(stringResource(R.string.add_cat)) }
     )
 }
 
