0a34ec1f1dcaa4b25aa5f327ce9dbf8c44f8f261
Ruben Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-07-22 16:09:08 +0200

    Improved breed item expanding animation

--- app/src/main/java/cat/rubenzu03/catbrary/ui/composables/BreedInfoItem.kt
+++ app/src/main/java/cat/rubenzu03/catbrary/ui/composables/BreedInfoItem.kt
@@ -1,5 +1,12 @@
 package cat.rubenzu03.catbrary.ui.composables
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -37,14 +44,28 @@ fun BreedInfoItem(breed: CatBreedInfo, modifier: Modifier = Modifier) {
                     contentDescription = if (expanded) "Mostrar menos" else "Mostrar más"
                 )
             }
-            if (expanded) {
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = breed.description, style = MaterialTheme.typography.bodySmall)
-                //breed.lifeSpan?.let { Text(text = "Esperanza de vida: $it años", style = MaterialTheme.typography.bodySmall) }
-                Text(text = "Inteligencia: ${breed.intelligence}", style = MaterialTheme.typography.bodySmall)
-                Text(text = "Adaptabilidad: ${breed.adaptability}", style = MaterialTheme.typography.bodySmall)
-                Text(text = "Nivel de afecto: ${breed.affectionLevel}", style = MaterialTheme.typography.bodySmall)
-                Text(text = "Amigable con niños: ${breed.childFriendly}", style = MaterialTheme.typography.bodySmall)
+            AnimatedVisibility(
+                visible = expanded,
+                enter = expandVertically(
+                    animationSpec = tween(300, easing = FastOutSlowInEasing)
+                ) + fadeIn(
+                    animationSpec = tween(300, easing = FastOutSlowInEasing)
+                ),
+                exit = shrinkVertically(
+                    animationSpec = tween(300, easing = FastOutSlowInEasing)
+                ) + fadeOut(
+                    animationSpec = tween(300, easing = FastOutSlowInEasing)
+                )
+            ) {
+                Column {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = breed.description ?: "", style = MaterialTheme.typography.bodySmall)
+                    //breed.lifeSpan?.let { Text(text = "Esperanza de vida: $it años", style = MaterialTheme.typography.bodySmall) }
+                    Text(text = "Inteligencia: ${breed.intelligence}", style = MaterialTheme.typography.bodySmall)
+                    Text(text = "Adaptabilidad: ${breed.adaptability}", style = MaterialTheme.typography.bodySmall)
+                    Text(text = "Nivel de afecto: ${breed.affectionLevel}", style = MaterialTheme.typography.bodySmall)
+                    Text(text = "Amigable con niños: ${breed.childFriendly}", style = MaterialTheme.typography.bodySmall)
+                }
             }
         }
     }
